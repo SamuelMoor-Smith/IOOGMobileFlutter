bc550612bd57b01d8c7969d9882558f3687fbfe6
Samuel Moor-Smith <samuel.moorsmith@mail.utoronto.ca>
2023-03-26 15:17:38 -0400

    merges

--- lib/components/field_widgets/multiple_choice/radio_button.dart
+++ lib/components/field_widgets/multiple_choice/radio_button.dart
@@ -1,14 +1,26 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:namer_app/components/field_widgets/multiple_choice/multiple_choice.dart';
 import 'package:namer_app/models/choice.dart';
 import 'package:namer_app/style/text_styles.dart';
 
+import '../../../main.dart';
 import '../../../models/field/field.dart';
 import '../../../style/borders.dart';
 
 class IOOGRadioGroup extends IOOGMultipleChoice {
 
-  IOOGRadioGroup ({ Key? key, required Field field, required Set<Choice> choices }): super(key: key, field: field, choices: choices);
+  IOOGRadioGroup ({ 
+    Key? key,
+    required Field field,
+    required Set<Choice> choices,
+  }) : super(
+          key: key,
+          choices: choices,
+          field: field,
+        );
 
   @override
   State<IOOGRadioGroup> createState() => _IOOGRadioGroup();
@@ -30,26 +42,33 @@ class _IOOGRadioGroup extends State<IOOGRadioGroup> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0),
-      decoration: bordered,
-      child: Column(
-        children: [
-          ListTile(
-            title: Text(widget.getLabelText(), style: primaryTextStyle(),),
-            visualDensity: VisualDensity(vertical: -4)
-          ), ...widget.getChoices().map((choice) => 
-                RadioListTile<Choice>(
-                  title: Text(choice.name, style: primaryTextStyle(),),
-                  value: choice,
-                  groupValue: widget.getSelectedChoices().first, // Only ever 1 choice selected
-                  onChanged: (Choice? value) {
-                    setState(() {
-                      widget.selectChoice(value!);
-                    });
-                  },
+    return 
+      FormBuilderField(
+        name: widget.getFieldName(),
+        builder: (FormFieldState<dynamic> state) {
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0),
+            decoration: bordered,
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(widget.getLabelText(), style: primaryTextStyle(),),
                   visualDensity: VisualDensity(vertical: -4)
-                )).toList()
-          ]));
+                ), ...widget.getChoices().map((choice) => 
+                      RadioListTile<Choice>(
+                        title: Text(choice.name, style: primaryTextStyle(),),
+                        value: choice,
+                        groupValue: widget.getSelectedChoices().first, // Only ever 1 choice selected
+                        onChanged: (Choice? value) {
+                          state.didChange(value);
+                          log(formKey.currentState!.fields as String);
+                          setState(() {
+                            widget.selectChoice(value!);
+                          });
+                        },
+                        visualDensity: VisualDensity(vertical: -4)
+                      )).toList()
+                ]));
+              });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:namer_app/pages/login/login.dart';
 
 void main() => runApp(const MyApp());
+
+final formKey = GlobalKey<FormBuilderState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
